a8b92e15785ccba118e28163f95eb091e3a607bb
Lance <Lance@thelpstory.com>
2016-09-17 07:36:34 -0600

    cors

--- server/app.js
+++ server/app.js
@@ -16,6 +16,15 @@ var routes = require('./logic/refactor')
 // *** express instance *** //
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
+
 // *** mongoose *** //
 mongoose.connect('mongodb://localhost/node-football', function (err, res) {
     if (err) {
