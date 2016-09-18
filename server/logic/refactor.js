c14872cf1c7f6d47b612a07a2de997ddfa7c5a2c
Lance <Lance@thelpstory.com>
2016-09-17 20:24:05 -0600

    Changed Database url

--- server/logic/refactor.js
+++ server/logic/refactor.js
@@ -41,7 +41,7 @@ routes.get('/teams', function (request, response) {
             if(res == undefined){
               console.log('Created = undefined')
               deferred.resolve(getStats(teams))
-            } else if ((Date.now()-res.created)/1000/60 <  2) {
+            } else if ((Date.now()-res.created)/1000/60 <  24 * 60) {
               console.log((Date.now()-res.created)/1000/60)
               deferred.resolve(res.teams)
               console.log('Created != undefined')
